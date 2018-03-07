2aca93f826f615dc0cc5eff05c136e95344b5047
AlfredZhong <kqzhong@gmail.com>
2018-03-07 10:10:32 +0800

    add benchmark for Two Sum

--- TwoSum/algo_test.go
+++ TwoSum/algo_test.go
@@ -1,6 +1,7 @@
 package twoSum
 
 import (
+	"math/rand"
 	"reflect"
 	"testing"
 )
@@ -24,5 +25,37 @@ func Test_twoSum(t *testing.T) {
 				t.Errorf("twoSum() = %v, want %v", got, tt.want)
 			}
 		})
+	}
+}
+
+func Benchmark_twoSum(b *testing.B) {
+	size := 10000
+	numMax := 100
+
+	nums := make([]int, size)
+	for i := range nums {
+		nums[i] = rand.Intn(numMax)
+	}
+	target := rand.Intn(numMax)
+	p1 := rand.Intn(numMax)
+	p2 := target - p1
+
+	i1 := rand.Intn(size)
+	i2 := i1
+	for i2 == i1 {
+		i2 = rand.Intn(size)
+	}
+	nums[i1] = p1
+	nums[i2] = p2
+
+	for i := 0; i < b.N; i++ {
+		r := twoSum(nums, target)
+		if r == nil || len(r) != 2 {
+			b.Errorf("result %v is not illegal", r)
+			continue
+		}
+		if nums[r[0]]+nums[r[1]] != target {
+			b.Errorf("%d + %d != %d", nums[r[0]], nums[r[1]], target)
+		}
 	}
 }
