67594491d17305aaabe1431cec990f1263d12085
AlfredZhong <kqzhong@gmail.com>
2018-03-09 22:44:04 +0800

    231. Power of Two

---
+++ PowerOfTwo/algo.go
@@ -0,0 +1,5 @@
+package algo
+
+func isPowerOfTwo(n int) bool {
+    return n > 0 && n & (n - 1) == 0
+}
---
+++ PowerOfTwo/algo_test.go
@@ -0,0 +1,25 @@
+package algo
+
+import "testing"
+
+func Test_isPowerOfTwo(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"t1", args{2}, true},
+		{"t2", args{6}, false},
+		{"t3", args{8}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPowerOfTwo(tt.args.n); got != tt.want {
+				t.Errorf("isPowerOfTwo() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
